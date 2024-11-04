6962a1377edb924bcee2afcf13090b6f951951ac
Victor Rodrigo Cruz Delgado <vrcruz@alcaldiabogota.gov.co>
2024-11-04 17:27:34 -0500

    change test

--- src/test/java/co/jave/testing/config/timezone/HibernateTimeZoneIT.java
+++ src/test/java/co/jave/testing/config/timezone/HibernateTimeZoneIT.java
@@ -43,8 +43,8 @@ class HibernateTimeZoneIT {
         dateTimeWrapper.setLocalDateTime(LocalDateTime.parse("2014-11-12T07:50:00.0"));
         dateTimeWrapper.setOffsetDateTime(OffsetDateTime.parse("2011-12-14T08:30:00.0Z"));
         dateTimeWrapper.setZonedDateTime(ZonedDateTime.parse("2011-12-14T08:30:00.0Z"));
-        dateTimeWrapper.setLocalTime(LocalTime.parse("14:30:00"));
-        dateTimeWrapper.setOffsetTime(OffsetTime.parse("14:30:00+02:00"));
+        dateTimeWrapper.setLocalTime(LocalTime.parse("12:30:00"));
+        dateTimeWrapper.setOffsetTime(OffsetTime.parse("12:30:00+02:00"));
         dateTimeWrapper.setLocalDate(LocalDate.parse("2016-09-10"));
 
         dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.S").withZone(ZoneId.of(zoneId));
